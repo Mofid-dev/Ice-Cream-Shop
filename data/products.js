387f23617dffe57348c5a9861193419b7c86fb4c
Mofid-dev <maxamedckarin90@gmial.com>
2025-12-30 16:53:46 +0300

    usind product.js file

---
+++ data/products.js
@@ -0,0 +1,49 @@
+const products = [{
+    image: "images/products/product-1.png",
+    name: "strawberry",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 14.99,
+    rating: 4.5
+}, {
+    image: "images/products/product-2.png",
+    name: "Vanilla",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 2499,
+    rating: 5
+}, {
+    image: "images/products/product-3.png",
+    name: "Buttered Pecan",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 1150,
+    rating: 4.5
+}, {
+    image: "images/products/product-4.webp",
+    name: "Buttered Pecan",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 1899,
+    rating: 5
+}, {
+    image: "images/products/product-5.png",
+    name: "Cookie Dough",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 1999,
+    rating: 5
+}, {
+    image: "images/products/product-6.png",
+    name: "Moose Tracks",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 999,
+    rating: 4
+}, {
+    image: "images/products/product-7.png",
+    name: "Buttered Pecan",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 1599,
+    rating: 4.5
+}, {
+    image: "images/products/product-8.png",
+    name: "Mint Chocolate",
+    detail: "Associate with being lively, creative and dramatic",
+    price: 899,
+    rating: 4
+}]
--- script.js
+++ script.js
@@ -1,60 +1,9 @@
 const menuIcon = document.querySelector(".menu-icon");
 const navLinks = document.querySelector(".nav-bar-links");
-const productGrid = document.querySelector(".product-grid")
 
-const products = [{
-    image: "images/products/product-1.png",
-    name: "strawberry",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 14.99,
-    rating: 4.5
-}, {
-    image: "images/products/product-2.png",
-    name: "Vanilla",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 2499,
-    rating: 5
-}, {
-    image: "images/products/product-3.png",
-    name: "Buttered Pecan",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 1150,
-    rating: 4.5
-}, {
-    image: "images/products/product-4.webp",
-    name: "Buttered Pecan",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 1899,
-    rating: 5
-}, {
-    image: "images/products/product-5.png",
-    name: "Cookie Dough",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 1999,
-    rating: 5
-}, {
-    image: "images/products/product-6.png",
-    name: "Moose Tracks",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 999,
-    rating: 4
-}, {
-    image: "images/products/product-7.png",
-    name: "Buttered Pecan",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 1599,
-    rating: 4.5
-}, {
-    image: "images/products/product-8.png",
-    name: "Mint Chocolate",
-    detail: "Associate with being lively, creative and dramatic",
-    price: 899,
-    rating: 4
-}]
-
-
+let productsHTML = ''
 products.forEach(product => {
-    const html = `
+    productsHTML += `
     <div class="product-container">
         <div class="product-image">
             <img src="${product.image}" alt="">
@@ -69,10 +18,10 @@ products.forEach(product => {
             <button class="add-to-cart">Add to cart</button>
         </div>
     </div>
-    `
-    productGrid.innerHTML += html
-    console.log(product)
+    `;
 });
+
+document.querySelector(".product-grid").innerHTML = productsHTML
 
 menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle("open");
